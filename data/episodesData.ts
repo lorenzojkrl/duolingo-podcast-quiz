94a13e4f59680e3df359445fc860f3027580b5ff
Lorenzo Zarantonello <lorenzozar@gmail.com>
2024-10-14 21:57:38 +0300

    chore: new data

---
+++ data/episodesData.ts
@@ -0,0 +1,20 @@
+export interface EpisodeInfo {
+  id: string;
+  title: string;
+}
+
+export const episodes: EpisodeInfo[] = [
+  {
+    id: "151",
+    title: "Episode 151: Duo's Film Club - Fresa y chocolate",
+  },
+  {
+    id: "150",
+    title: "Episode 150: Duo's Film Club - Coco",
+  },
+  {
+    id: "149",
+    title:
+      "Episode 149: Duo's Film Club - Mujeres al borde de un ataque de nervios",
+  },
+];
